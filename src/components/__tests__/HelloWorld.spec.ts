b4f79aca4f4d5bddfbe98d3ce20b2da8271cbaf4
Takami Hirata <26153825+takami-h@users.noreply.github.com>
2022-07-24 14:44:47 +0900

    test: introduce testing-library/vue

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,11 @@
-import { describe, it, expect } from 'vitest'
+import { render } from "@testing-library/vue";
+import "@testing-library/jest-dom";
 
-import { mount } from '@vue/test-utils'
 import HelloWorld from '../HelloWorld.vue'
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+    const screen = render(HelloWorld, { props: { msg: "Hello Vitest" } });
+    expect(screen.getByText("Hello Vitest")).toBeInTheDocument();
   })
 })
